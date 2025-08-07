64532fafa3fc01741b6534e97a92ef38c612f267
Chaitanyasai21 <chaitanyasai.juluru@gmail.com>
2025-08-07 15:09:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (67.34%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] ar) {
+        for(int i = ar.length - 1; i >= 0; i--){
+            ar[i] += 1;
+            if(ar[i] <= 9){
+                return ar;
+            }
+            ar[i] = 0;
+        }
+        int n = ar.length;
+        int []ans = new int[n+1];
+        ans[0] = 1;
+        for(int i=1;i<=n;i++)
+            ans[i] = ar[i-1];
+        return ans;
+    }
+}
