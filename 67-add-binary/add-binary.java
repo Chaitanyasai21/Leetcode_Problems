e13b76b7cb9f2cf89a2c4ed7a7ad6cd9a0d8f329
Chaitanyasai21 <chaitanyasai.juluru@gmail.com>
2025-08-07 16:14:34 +0530

    Time: 8 ms (7.03%) | Memory: 45.2 MB (5.60%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -14,47 +14,17 @@ class Solution {
         String s = "";
         int c = 0;
         for(int i = a.length() - 1; i >= 0; i--){
-            int a1 = a.charAt(i) - '0';
+            int a1 = a.charAt(i) -'0';
             int b1 = b.charAt(i) - '0';
-            if(a1 == 1 && b1 == 1){
-                if(c == 1){
-                    s += "1";
-                }
-                else{
-                    s += "0";
-                }
-                c = 1;
-            }
-            else{
-                if(a1 == 0 && b1 == 0){
-                    if(c == 1){
-                        s += "1";
-                        c = 0;
-                    }else{
-                        s+="0";
-                    }
-                }else{
-                    if(c == 1){
-                        s += "0";
-                    }else{
-                        s += "1";
-                        c = 0;
-                    }
-                }  
-            }
+            int sum = a1 + b1 + c;
+            s += (sum%2);
+            c = sum / 2;
         }
+        
         if(c == 1){
             s += "1";
         }
-        // System.out.println(s);
-         String res = new StringBuilder(s).reverse().toString();
-        // System.out.println(res);
-
-        // int i = 0;
-        // while (i < res.length() - 1 && res.charAt(i) == '0') {
-        //     i++;
-        // }
-        // System.out.println(i);
+        String res = new StringBuilder(s).reverse().toString();
         return res;
     }
 }
