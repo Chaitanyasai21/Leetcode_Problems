5b89d679fd0cee299b0a636e0fb49c11c3c604de
Chaitanyasai21 <chaitanyasai.juluru@gmail.com>
2025-10-16 19:19:26 +0530

    Time: 9 ms (2.62%) | Memory: 41.8 MB (5.90%) - LeetSync

---
+++ 1070-convert-to-base-2/convert-to-base-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String baseNeg2(int n) {
+        if (n == 0) return "0";
+
+        String s = "";
+        while (n != 0) {
+            int rem = n % -2;
+            if (rem < 0) {
+                rem += 2;
+                n = (n / -2) + 1;
+            } else {
+                n = n / -2;
+            }
+            s = rem + s;
+        }
+
+        System.out.println(s);
+        return s;
+    }
+}
